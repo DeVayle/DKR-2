fa8737baf5cbc43f01c1b09f1133959267884d42
DeVayle <144678017+DeVayle@users.noreply.github.com>
2023-11-22 20:22:27 +0300

    Add files via upload

---
+++ dkr2_1.pas
@@ -0,0 +1,18 @@
+﻿program dkr2_1;
+
+const
+  n = 10;
+  
+var
+  a: array [1..n] of integer;
+  i, s, sar: integer;
+  
+begin
+  writeln('Введите массив:');
+  for i:=1 to n do
+  begin
+    read(a[i]);
+    s+= a[i];
+  end;
+  writeln('Среднеарифметическое в массиве: ', sar:= s/n)
+end.
---
+++ dkr2_2.pas
@@ -0,0 +1,45 @@
+﻿program dkr2_2;
+
+var
+  s, ps, t: string;
+  i, iFirst, iLast, k: integer;
+
+begin
+  write('Введите строку: ');
+  readln(s);
+  write('Введите подстроку: ');
+  readln(ps);
+  
+  // нахождение первого и последнего вхождения //
+  i:= 1;
+  while (i<=length(s)) and (length(ps)>0) do
+  begin
+    k:= 0;
+    while ((i + k)<=length(s)) and (k<length(ps)) and (s[i+k] = ps[k+1]) do inc(k);
+    if k=Length(ps) then
+    begin
+      if iFirst=0 then iFirst:= i
+      else iLast:= i;
+      inc(i, k);
+    end
+    else inc(i);
+  end;
+  
+  // удаление подстрок из строки //
+  t:= '';
+  if iFirst>0 then
+  begin
+    i:= 1;
+    while i<=Length(s) do
+    begin
+      if (i=iFirst) or (i=iLast) then inc(i, Length(ps))
+      else
+      begin
+        t:= t+s[i];
+        inc(i);
+      end;
+    end;  
+    s:= t;
+  end;
+  writeln(s);
+end.
